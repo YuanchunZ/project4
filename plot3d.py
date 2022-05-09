e71fa69e23e8005b8fbf3833a8c6e600bb82e713
YuanchunZ <102001971+YuanchunZ@users.noreply.github.com>
2022-05-08 20:07:19 -0500

    Add files via upload

---
+++ plot3d.py
@@ -0,0 +1,84 @@
+from sklearn.preprocessing import normalize
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.load("E:/work/car_logo_classification/feature/train_feature_reduction3.npy")
+y = np.load("E:/work/car_logo_classification/feature/train_label.npy")
+a  = set()
+for i in y:
+    a.add(i)
+print(len(a))
+print(a)
+tmp_colors = {'aliceblue':            '#F0F8FF',
+'antiquewhite':         '#FAEBD7',
+'aqua':                 '#00FFFF',
+'aquamarine':           '#7FFFD4',
+'azure':                '#F0FFFF',
+'beige':                '#F5F5DC',
+'bisque':               '#FFE4C4',
+'black':                '#000000',
+'blanchedalmond':       '#FFEBCD',
+'blue':                 '#0000FF',
+'blueviolet':           '#8A2BE2',
+'brown':                '#A52A2A',
+'burlywood':            '#DEB887',
+'cadetblue':            '#5F9EA0',
+'chartreuse':           '#7FFF00',
+'chocolate':            '#D2691E',
+'coral':                '#FF7F50',
+'cornflowerblue':       '#6495ED',
+'cornsilk':             '#FFF8DC',
+'crimson':              '#DC143C',
+'cyan':                 '#00FFFF',
+'darkblue':             '#00008B',
+'darkcyan':             '#008B8B',
+'darkgoldenrod':        '#B8860B',
+'darkgray':             '#A9A9A9',
+'darkgreen':            '#006400',
+'darkkhaki':            '#BDB76B',
+'darkmagenta':          '#8B008B',
+'darkolivegreen':       '#556B2F',
+'darkorange':           '#FF8C00',
+'darkorchid':           '#9932CC',
+'darkred':              '#8B0000',
+'darksalmon':           '#E9967A',
+'darkseagreen':         '#8FBC8F',
+'darkslateblue':        '#483D8B',
+'darkslategray':        '#2F4F4F',
+'darkturquoise':        '#00CED1',
+'darkviolet':           '#9400D3',
+'deeppink':             '#FF1493',
+'deepskyblue':          '#00BFFF',
+'dimgray':              '#696969',
+'dodgerblue':           '#1E90FF',
+'firebrick':            '#B22222',
+'floralwhite':          '#FFFAF0',
+'forestgreen':          '#228B22',
+'fuchsia':              '#FF00FF',
+'gainsboro':            '#DCDCDC',
+'ghostwhite':           '#F8F8FF',
+'gold':                 '#FFD700',
+'goldenrod':            '#DAA520',
+'gray':                 '#808080',
+'green':                '#008000',
+'greenyellow':          '#ADFF2F',
+'honeydew':             '#F0FFF0',
+'hotpink':              '#FF69B4',
+'indianred':            '#CD5C5C',
+'indigo':               '#4B0082',
+'ivory':                '#FFFFF0',
+'khaki':                '#F0E68C'}
+colors = []
+for k,v in tmp_colors.items():
+    colors.append(k)
+
+# print(x.shape[0])
+x = normalize(x, axis=0, norm='max')
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+for i in range(x.shape[0]):
+    # fig = plt.figure()
+    ax.scatter3D(x[i, 0], x[i, 1], x[i, 2], 
+            color=colors[y[i]])
+plt.show()
+plt.savefig("3d.png")
